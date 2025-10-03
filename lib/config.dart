88990bad41bb850f2a693551c3992a59bcb28a38
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-10-03 09:36:34 +0700

    Update VPS Baru

--- lib/config.dart
+++ lib/config.dart
@@ -2,6 +2,6 @@ class Config {
   // Ambil dari --dart-define, kalau gak ada pakai default (misal localhost)
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'http://45.127.134.179:4000/api',
+    defaultValue: 'http://103.94.238.252:4000/api',
   );
 }
